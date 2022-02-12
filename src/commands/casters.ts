6e9ea6339cedad30d4b77a7dc256f529e27219a9
Cesar Solano <csolanoc@unal.edu.co>
2022-02-11 19:53:25 -0500

    fixed casters

--- src/commands/casters.ts
+++ src/commands/casters.ts
@@ -1,16 +1,15 @@
 import { horaroAPI } from "../cfg/horaro-api";
 import { twitchAPI } from "../cfg/twitch-api";
 
-module.exports = async(args: any[], horarioAPI:horaroAPI, twitchAPI:twitchAPI) => {
+module.exports = async (args: any[], horarioAPI: horaroAPI, twitchAPI: twitchAPI) => {
     let text;
     if (!horarioAPI.getActive()) return 'No ha iniciado el horario.';
 
     // TO DO: implementar flags
 
 
-    let row = await horarioAPI.getRowById(horarioAPI.getCounter())
-    let column = await horarioAPI.getColumnByName('Comentaristas');
+    let row = await horarioAPI.getRowById(horarioAPI.getCounter()-1)
+    let column = await horarioAPI.getColumnByName('hiddenComs');
     text = row.data[column]
-
     return horarioAPI.formatString(text, true);
 }
